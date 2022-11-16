0873e7cc53d6f48cad9a5b1a5b4b05721972f3b7
waseemalnounou <waseem.alnono@gmail.com>
2022-11-16 14:32:07 +1000

    Add API Endpoint to convert String to number

---
+++ Exercises/Controllers/GetNumberController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Exercises.Utils;
+using System.Text.RegularExpressions;
+
+namespace Exercises.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConvertStringToNumberController : ControllerBase
+{
+    private readonly ILogger<ConvertStringToNumberController> _logger;
+
+    public ConvertStringToNumberController(ILogger<ConvertStringToNumberController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("{input}", Name = "GetNumberFromString")]
+    public JsonResult Post(string input)
+    {   
+        // validation
+        if(input.Length > 0)
+        {
+        // check if input is number
+        Boolean isNumber =  Regex.IsMatch(input , @"^\d+$|^[+-]?\d+$");
+        if (!isNumber){
+            Response.StatusCode = StatusCodes.Status400BadRequest;
+            return new JsonResult(new { error = "Invalid number"});
+        } else {
+            // check if there is sign without number
+            if((input[0] == '-' || input[0] == '+') && input.Length == 1){
+                Response.StatusCode = StatusCodes.Status400BadRequest;
+                return new JsonResult(new { error = "Invalid number"});
+            }
+
+            StringAsNumber str = new StringAsNumber(input);
+            var val = str.ConvertToNumber();
+            Response.StatusCode = StatusCodes.Status200OK;
+            return new JsonResult(new { value = val});
+        }
+    } else {
+        Response.StatusCode = StatusCodes.Status400BadRequest;
+        return new JsonResult(new { error = "Input should not be empty"});
+    }
+    }
+}
---
+++ Exercises/Utils/StrToNum.cs
@@ -0,0 +1,47 @@
+namespace Exercises.Utils;
+
+public class StringAsNumber {
+    string text;
+    public StringAsNumber(string text){
+        this.text = text;
+    }
+
+    public Boolean IsNum(char num){
+        return Char.IsDigit(num);
+    }
+
+    public int ConvertToNumber(){
+        Boolean isNegative = false;
+        string newText = text;
+        int number = 0;
+        
+        if(text.Length > 0)
+        {
+            // handle positive/negative input
+            if(text[0] == '-' || text[0] == '+'){
+                if(text[0] == '-')
+                isNegative = true;
+
+                newText = text.Substring(1);
+                if (newText == ""){
+                    throw new Exception("Invalid Number");
+                }
+            }
+
+            for (int i = 0; i< newText.Length; i++)
+            {
+                // subtract 48 from the ASCII value of the character will give the digit for the character
+                if (IsNum(newText[i]))
+                number = number * 10 + ((int)newText[i] - 48);
+                else{
+                    throw new Exception("Invalid Number");
+                }
+            }
+
+        } else {
+            throw new Exception("Input Length sould be greater than 0");
+        }
+
+        return isNegative ? number * -1 : number;
+    }
+}
